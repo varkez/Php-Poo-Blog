8a1039e4068433914632028afd6091def7a4aa98
varkez <31601865+varkez@users.noreply.github.com>
2018-04-26 00:22:04 +0200

    Update Config.php

--- core/Config.php
+++ core/Config.php
@@ -27,8 +27,8 @@ class Config{
         return self::$_instance;
     }
 
-    /** Permet d'obtenir la valeur de la configuration avec getInstance()->get('db_name');
-     * @param $key clef a récupérer dans tableau config/config.php
+    /** Permet d'obtenir la valeur de la configuration a la DB avec getInstance()->get('db_name');
+     * @param $key clef a récupérer dans le tableau (core/config.php)
      */
     public function get($key){
         if(!isset($this->settings[$key])){
